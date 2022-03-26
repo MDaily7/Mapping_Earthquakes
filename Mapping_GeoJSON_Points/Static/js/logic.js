f2a8f0f1c56c668c1b84e4a27c1eac2d32a01f8c
MDaily7 <mdaily622@gmail.com>
2022-03-26 12:53:19 -0700

    added single point with popups from geojson

--- Mapping_GeoJSON_Points/Static/js/logic.js
+++ Mapping_GeoJSON_Points/Static/js/logic.js
@@ -23,10 +23,20 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-// GeoJSON layer
-L.geoJSON(sanFranAirport).addTo(map);
+// GeoJSON layer with pointToLayer
+// L.geoJSON(sanFranAirport, {
+//     pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//         .bindPopup(`<h2> ${feature.properties.name} </h2><hr><h3>${feature.properties.city}, ${feature.properties.country}</h3>`);
+// }}).addTo(map);
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup(`<h2> Airport Code:${layer.feature.properties.faa}</h2><hr><h3>${layer.feature.properties.name}</h3>`);
+}}).addTo(map);
 // create tile layer that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
